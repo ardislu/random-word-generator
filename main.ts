cf5c41b2ce62f9b1fb187f0041e52754f3c16e0e
Ardis Lu <ardisglu@gmail.com>
2023-06-15 20:16:45 -0700

    Update dependency

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.186.0/flags/mod.ts";
+import { parse } from "https://deno.land/std@0.192.0/flags/mod.ts";
 
 const { words, separator, length } = parse(Deno.args, {
   default: {

af85ee48fe6ed79520ace9e7bd7fd4bbfc505ccc
Ardis Lu <ardisglu@gmail.com>
2023-10-03 20:41:41 -0700

    Upgrade dependency

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { parse } from "https://deno.land/std@0.202.0/flags/mod.ts";
+import { parse } from "https://deno.land/std@0.203.0/flags/mod.ts";
 
 const { words, separator, length } = parse(Deno.args, {
   default: {
